8375ff58b4e963a75609c57ee2970c5deb85880d
Zer01100 <123416197+Zer01100@users.noreply.github.com>
2024-04-14 17:51:19 +0200

    Commit 3

--- Minesweeper/Minesweeper/GameEasy.xaml.cs
+++ Minesweeper/Minesweeper/GameEasy.xaml.cs
@@ -1,47 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Minesweeper
 {
     public partial class GameEasy : Window
     {
+
+        private DispatcherTimer timer;
+        private int seconds;
         public GameEasy()
         {
             InitializeComponent();
+            InitializeTimer();
             GameBoard gameBoard = new GameBoard(8, 8, 10);
 
-
-            // Przypisz kolekcję obiektów CellData do ItemsSource gameBoardArray
             gameBoardArray.ItemsSource = gameBoard.Board;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ButtonUncover(object sender, RoutedEventArgs e)
         {
-            // Znajdź nadrzędny Grid dla przycisku
             var grid = FindParent<Grid>((Button)sender);
 
-            // Znajdź pierwszy przycisk w kontenerze
             var button = (Button)VisualTreeHelper.GetChild(grid, 0);
-            button.Visibility = Visibility.Collapsed; // Schowaj przycisk
+            button.Visibility = Visibility.Collapsed;
 
-            // Znajdź drugi element (TextBlock) w kontenerze
             var textBlock = (TextBlock)VisualTreeHelper.GetChild(grid, 1);
-            textBlock.Visibility = Visibility.Visible; // Pokaż TextBlock
+            textBlock.Visibility = Visibility.Visible;
+
+            if(textBlock.Text == "0")
+            {
+                UncoverZeroes(grid);
+            }
+            else if(textBlock.Text == "10")
+            {
+                //endGame();
+            }
         }
 
-        private DependencyObject FindParent<TDependencyObject>(DependencyObject child)
+        private void UncoverZeroes(Grid grid)
+        {
+            var row = Grid.GetRow(grid);
+            var column = Grid.GetColumn(grid);
+
+
+            for (int i = Math.Max(0, row - 1); i <= Math.Min(7, row + 1); i++)
+            {
+                for (int j = Math.Max(0, column - 1); j <= Math.Min(7, column + 1); j++)
+                {
+                    var neighboringGrid = gameBoardArray.ItemContainerGenerator.ContainerFromIndex(i * 8 + j) as Grid;
+                    if (neighboringGrid != null)
+                    {
+                        var neighboringButton = (Button)VisualTreeHelper.GetChild(neighboringGrid, 0);
+                        var neighboringTextBlock = (TextBlock)VisualTreeHelper.GetChild(neighboringGrid, 1);
+
+                        if (neighboringButton.Visibility == Visibility.Visible && neighboringTextBlock.Text == "0")
+                        {
+                            neighboringButton.Visibility = Visibility.Collapsed;
+                            neighboringTextBlock.Visibility = Visibility.Visible;
+                            UncoverZeroes(neighboringGrid);
+                        }
+                    }
+                }
+            }
+        }
+
+        private Grid FindParent<TDependencyObject>(DependencyObject child)
         {
             DependencyObject parent = VisualTreeHelper.GetParent(child);
 
@@ -50,7 +77,61 @@ namespace Minesweeper
                 parent = VisualTreeHelper.GetParent(parent);
             }
 
-            return parent as DependencyObject;
+            return parent as Grid;
+        }
+
+        private void ButtonFlagPlacement(object sender, MouseButtonEventArgs e)
+        {
+            var button = sender as Button;
+
+                if ((button.Content.ToString() == "0") && (int.Parse(FlagCounter.Text) > 0))
+                {
+                    button.Content = "1";
+                    FlagCounter.Text = (int.Parse(FlagCounter.Text) - 1).ToString("D3");
+                }
+                else if ((button.Content.ToString()) == "1")
+                {
+                    button.Content = "0";
+                    FlagCounter.Text = (int.Parse(FlagCounter.Text) + 1).ToString("D3");
+                }
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            if (seconds < 999)
+            {
+                seconds++;
+                TimerTextBlock.Text = seconds.ToString("D3");
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+
+        private void ButtonBack(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainMenu = new MainWindow();
+            MainMenu.Show();
+            this.Close();
+        }
+
+        private void GameRestart(object sender, MouseButtonEventArgs e)
+        {
+            GameEasy GameWindow = new GameEasy();
+            GameWindow.Left = this.Left;
+            GameWindow.Top = this.Top;
+            GameWindow.Show();
+            this.Close();
         }
     }
 }
